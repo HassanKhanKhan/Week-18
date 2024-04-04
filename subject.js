8c42e200f3fc4e18d620e4f7204525bdaab243d2
Hassan Khan <hk800@live.mdx.ac.uk>
2024-04-04 15:29:29 +0400

    Updated

--- subject.js
+++ subject.js
@@ -1,69 +1,74 @@
 let subjects = [{
 
-    lesson: "Faber-Castell Whiteboard Marker Bullet Set Of 4Pc + Duster, ASSORTED, 1587D",
-    location: 'Amazon.ae',
-    price: "5000",
-    spaces: 3,
-    image: "img/iphone15.png"
-},
-
-{
-    lesson: "Brother PT-H110 Label Printer For Home And Small Office",
-    location: 'Global Village, Amazon.ae',
-    price: "4000",
-    spaces: 15,
-    image: "img/iphone14.png"
+    lesson: "English Language",
+    location: 'Burj Khalifa',
+    price: "500",
+    spaces: 5,
+    image: "img/englishlanguage.jpg"
 },
 {
-    lesson: "ALBA RADO Box File (8cm, Pack of 10 Pieces)",
-    location: 'Dubai Mall,Amazon.ae',
-    price: "1500",
-    spaces: 50,
-    image: "img/iphone13.png"
-},
-{
-    lesson: "Maxi POLYPROPYLENE DOUBLE CLIPBOARD FOOL SCAP BLACK, DCBFBK",
-    location: 'Ski Dubai,Amazon.ae',
-    price: "2000",
-    spaces: 45,
-    image: "img/iphone 12.png"
-},
-{
-    lesson: "ECVV 8pcs/Set Practical School Stationery Set,Geometry Set, Rulers,Protractor,Compass,Pencil Lead Refills,Pencil,Eraser for Student Supplies",
-    location: 'Dubai Garden,Amazon.ae',
+    lesson: "Swimming",
+    location: 'Burj Al Arab',
     price: "3000",
     spaces: 5,
-    image: "img/iphone11.png"
+    image: "img/swimming.jpg"
 },
 {
-    lesson: "80 COLOUR Graphic pen Professional Design Art Twin Tip Marker Pen Series Graphic Pen Manga Graphic W/bag Art Twin Marker Pen drawing markers",
-    location: 'Down Town,Amazon.ae',
+    lesson: "Sports activity",
+    location: 'Global Village',
+    price: "4000",
+    spaces: 5,
+    image: "img/sportactivity.png"
+},
+{
+    lesson: "Ice skating",
+    location: 'Dubai Mall',
+    price: "1500",
+    spaces: 5,
+    image: "img/icekating.png"
+},
+{
+    lesson: "Futsal",
+    location: 'Ski Dubai',
+    price: "2000",
+    spaces: 5,
+    image: "img/futsall.png"
+},
+{
+    lesson: "Arabic",
+    location: 'Dubai Garden',
+    price: "3000",
+    spaces: 5,
+    image: "img/arabic.jpeg"
+},
+{
+    lesson: "Indoor Games",
+    location: 'Down Town',
     price: "1000",
     spaces: 5,
-    image: "img/iphonex.png"
+    image: "img/indoorgames.jpg"
 },
 {
-    lesson: "Pre-Owned iPhone XR, 128GB, Blue",
-    location: 'Desert Safari,Amazon.ae',
+    lesson: "Outdoor Games",
+    location: 'Desert Safari',
     price: "800",
     spaces: 5,
-    image: "img/iphonexr.png"
+    image: "img/ouutdoorgames.png"
 },
 {
-    lesson: "(6 Compartments and 1 Drawer)",
-    location: 'Home town, Amazon.ae',
-    price: "2000",
-    spaces: 20,
-    image: "img/13mini.png"
+    lesson: "Movies",
+    location: 'Dubai Mall',
+    price: "500",
+    spaces: 5,
+    image: "img/movies.jpg"
 
 },
 {
-    lesson: "Nina Floral Stationery Set with A6",
-    location: 'Dubai Mall,Amazon.ae',
-    price: "3500",
-    spaces: 16,
-    image: "img/image copy 2.png"
+    lesson: "Practice a Skill",
+    location: 'Home town',
+    price: "200",
+    spaces: 5,
+    image: "img/practiceskils.png"
 
 },
-
 ]
