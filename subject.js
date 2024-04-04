d7e0a0ab2d50222f8314700a3c23eb0d17b33df7
Hassan Khan <hk800@live.mdx.ac.uk>
2024-04-04 14:39:02 +0400

    Updated

--- subject.js
+++ subject.js
@@ -1,6 +1,6 @@
 let subjects = [{
 
-    lesson: "Apple iPhone 15 Pro Max (512 GB) - Blue Titanium",
+    lesson: "Faber-Castell Whiteboard Marker Bullet Set Of 4Pc + Duster, ASSORTED, 1587D",
     location: 'Amazon.ae',
     price: "5000",
     spaces: 3,
@@ -8,35 +8,35 @@ let subjects = [{
 },
 
 {
-    lesson: "Apple iPhone 14 Pro (256 GB) - Purple",
+    lesson: "Brother PT-H110 Label Printer For Home And Small Office",
     location: 'Global Village, Amazon.ae',
     price: "4000",
     spaces: 15,
     image: "img/iphone14.png"
 },
 {
-    lesson: "Apple iPhone 13 Pro (256GB) - Sierra Blue (Renewed)",
+    lesson: "ALBA RADO Box File (8cm, Pack of 10 Pieces)",
     location: 'Dubai Mall,Amazon.ae',
     price: "1500",
     spaces: 50,
     image: "img/iphone13.png"
 },
 {
-    lesson: "Apple iPhone 12 Pro - 256GB - Graphite (Unlocked) Smartphone",
+    lesson: "Maxi POLYPROPYLENE DOUBLE CLIPBOARD FOOL SCAP BLACK, DCBFBK",
     location: 'Ski Dubai,Amazon.ae',
     price: "2000",
     spaces: 45,
     image: "img/iphone 12.png"
 },
 {
-    lesson: "Apple iPhone 11 (128GB) - White",
+    lesson: "ECVV 8pcs/Set Practical School Stationery Set,Geometry Set, Rulers,Protractor,Compass,Pencil Lead Refills,Pencil,Eraser for Student Supplies",
     location: 'Dubai Garden,Amazon.ae',
     price: "3000",
     spaces: 5,
     image: "img/iphone11.png"
 },
 {
-    lesson: "iphone X 64Gb Silvar (Renewed)",
+    lesson: "80 COLOUR Graphic pen Professional Design Art Twin Tip Marker Pen Series Graphic Pen Manga Graphic W/bag Art Twin Marker Pen drawing markers",
     location: 'Down Town,Amazon.ae',
     price: "1000",
     spaces: 5,
@@ -50,7 +50,7 @@ let subjects = [{
     image: "img/iphonexr.png"
 },
 {
-    lesson: "Apple iPhone 13 (256GB) - Midnight",
+    lesson: "(6 Compartments and 1 Drawer)",
     location: 'Home town, Amazon.ae',
     price: "2000",
     spaces: 20,
@@ -58,11 +58,11 @@ let subjects = [{
 
 },
 {
-    lesson: "Apple iPhone 15 Plus (128 GB) - Pink",
+    lesson: "Nina Floral Stationery Set with A6",
     location: 'Dubai Mall,Amazon.ae',
     price: "3500",
     spaces: 16,
-    image: "img/15plus.png"
+    image: "img/image copy 2.png"
 
 },
 
