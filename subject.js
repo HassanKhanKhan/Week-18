0cae0fb3066a888c7ea06c26cb1946a6c9435840
Hassan Khan <hk800@live.mdx.ac.uk>
2024-04-04 15:58:22 +0400

    Updated

--- subject.js
+++ subject.js
@@ -4,63 +4,63 @@ let subjects = [{
     location: 'Burj Khalifa',
     price: "500",
     spaces: 5,
-    image: "img/image copy.png"
+    image: "Images/image copy.png"
 },
 {
     lesson: "Swimming",
     location: 'Burj Al Arab',
     price: "3000",
     spaces: 5,
-    image: "img/image copy 2.png"
+    image: "Images/image copy 2.png"
 },
 {
     lesson: "Sports activity",
     location: 'Global Village',
     price: "4000",
     spaces: 5,
-    image: "img/image copy 3.png"
+    image: "Images/image copy 3.png"
 },
 {
     lesson: "Ice skating",
     location: 'Dubai Mall',
     price: "1500",
     spaces: 5,
-    image: "img/image copy 4.png"
+    image: "Images/image copy 4.png"
 },
 {
     lesson: "Futsal",
     location: 'Ski Dubai',
     price: "2000",
     spaces: 5,
-    image: "img/futsall.png"
+    image: "Images/image copy 9.png"
 },
 {
     lesson: "Arabic",
     location: 'Dubai Garden',
     price: "3000",
     spaces: 5,
-    image: "img/image.png"
+    image: "Images/image.png"
 },
 {
     lesson: "Indoor Games",
     location: 'Down Town',
     price: "1000",
     spaces: 5,
-    image: "img/image copy 5.png"
+    image: "Images/image copy 5.png"
 },
 {
     lesson: "Outdoor Games",
     location: 'Desert Safari',
     price: "800",
     spaces: 5,
-    image: "img/image copy 7.png"
+    image: "Images/image copy 7.png"
 },
 {
     lesson: "Movies",
     location: 'Dubai Mall',
     price: "500",
     spaces: 5,
-    image: "img/movies.jpg"
+    image: "Images/image copy 10.png"
 
 },
 {
@@ -68,7 +68,7 @@ let subjects = [{
     location: 'Home town',
     price: "200",
     spaces: 5,
-    image: "img/image copy 8.png"
+    image: "Images/image copy 8.png"
 
 },
 ]
